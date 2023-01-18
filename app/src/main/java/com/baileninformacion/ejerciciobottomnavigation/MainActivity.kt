a9b55e49145d63140f5c9cfe81ae0c7fcd618835
josemiguellorenzolara <josemiguel.lorenzolara1501@gmail.com>
2023-01-18 09:10:34 +0100

    Diseñando fragments, menu y navegador

--- app/src/main/java/com/baileninformacion/ejerciciobottomnavigation/MainActivity.kt
+++ app/src/main/java/com/baileninformacion/ejerciciobottomnavigation/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.nav_home, R.id.nav_asir, R.id.nav_dam, R.id.nav_daw
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
